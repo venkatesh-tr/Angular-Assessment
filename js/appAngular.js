f3546bd21477c0d30f08dcea104c419b4c3f263e
Prateek Kumar Srivastava <prateek@Prateeks-MacBook-Air.local>
2014-08-28 00:41:40 +0530

    hg-click

--- js/appAngular.js
+++ js/appAngular.js
@@ -5,7 +5,7 @@
 
 var testVar;
 var boxVar = 1;
- var t = "box1Left" + "1";
+var t = "box1Left" + "1";
 
 (function() {
 
@@ -139,8 +139,8 @@ var boxVar = 1;
             this.ActiveBoxData = GetTestCollection();
             this.testCollectionData = [];
             this.box1Left = 'box1Left';
-            this.box1a='box1a';
-            this.box1b='box1b';
+            this.box1a = 'box1a';
+            this.box1b = 'box1b';
             var j = 1;
             for (var i = 0; i < this.ActiveBoxData.length; i++) {
                 if (i < 3) {
@@ -149,10 +149,56 @@ var boxVar = 1;
                 j++;
 
             };
-            console.log(this.testCollectionData);
+            console.log(this.box1b);
+
+            this.shiftDown = function(downArrow, rightArrow, rightBox, downBox) {
+                var downArrowID = downArrow.id;
+                var rightArrowID = rightArrow;
+                var rightBoxID = rightBox;
+                var downBoxID = downBox;
+                // var numberOfRows = 3;
+                //rowHeads=new String{"firstRow", "secondRow", "thirdRow"};
+                if ($("#" + downArrowID + ".glyphicon-circle-arrow-down").is(":visible")) {
+                    // toggle all open rightShift to close
+                    $(".shiftLeft").animate({
+                        left: '-150px'
+                    }).hide(300);
+                    $(".show a").removeClass("glyphicon-circle-arrow-left").addClass("glyphicon-circle-arrow-right");
+                    // make all bottomShift to toggle
+                    $(".shiftDown").hide(10).css("height", "0");
+                    $(".show2 a").removeClass("glyphicon-circle-arrow-up").addClass("glyphicon-circle-arrow-down");
 
 
-            
+
+
+                    if ($(".row-in").is(":visible")) $("*").removeClass("row-in"); // if rows already there remove everything
+                    if ($(".col").is(":visible"));
+                    else {
+                        /*$("[rel~='firstRow']").wrapAll('<div class="col" />');  
+            $("[rel~='secondRow']").wrapAll('<div class="col" />');
+            $("[rel~='thirdRow']").wrapAll('<div class="col" />'); */
+                        //$("[rel~='fourthRow']").wrapAll('<div class="col" />');   
+                        //$("#"+ downBoxID +".shiftDown").hide(); 
+                        $("#" + downBoxID + ".shiftDown").slideDown(0);
+                    }
+                    $(".shiftLeft").hide();
+                    //$("#"+ downBoxID +".shiftDown").css("top","-130px");
+                    $("#" + downBoxID + ".shiftDown").css("display", "block");
+                    //$("#"+ downBoxID +".shiftDown").show(1000);
+                    $("#" + downBoxID + ".shiftDown").css("top", "0");
+                    $("#" + downBoxID + ".shiftDown").animate({
+                        height: '80px'
+                    });
+                    $("#" + downArrowID).removeClass("glyphicon-circle-arrow-down").addClass("glyphicon-circle-arrow-up");
+                    $("#" + rightArrowID).removeClass("glyphicon-circle-arrow-left").addClass("glyphicon-circle-arrow-right");
+                } else {
+                    $(".shiftDown").animate({
+                        height: '0'
+                    }).hide(10);
+                    $("#" + downArrowID).removeClass("glyphicon-circle-arrow-up").addClass("glyphicon-circle-arrow-down");
+                }
+            };
+
         }
     ])
 
